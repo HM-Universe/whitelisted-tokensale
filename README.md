b1c21f526e04546e191cfb1f44561bd9e73f260c
unknown <hideaki.mikitani@outlook.com>
2022-07-19 20:15:24 +0900

    udate

--- README.md
+++ README.md
@@ -9,9 +9,6 @@ Smart contracts have Administrators and Managers roles. These roles are designed
 - set a wallet where tokens received from the sale will be sent;
 - pause tokens sale if necessary.
 
-:construction: **[@whitelisted-tokensale](https://github.com/galtproject/whitelisted-tokensale) stage: :tada:Ethereum Mainnet:tada:**
-
-:bomb: **Security review status: At the moment, [@whitelisted-tokensale](https://github.com/galtproject/whitelisted-tokensale) contracts are deployed on the Ethereum mainnet. Nonetheless it is still experimental software that has not yet been publicly audited. Use it on your own risk!**
 
 ## Features
 - many Tokensale can use one Registry smart contract to check customers to be in whitelist;
---
+++ helpers/deploy.js
@@ -0,0 +1,34 @@
+const { contract, web3: ozWeb3 } = require('@openzeppelin/test-environment');
+const { ZWeb3, SimpleProject, Contracts } = require('@openzeppelin/upgrades');
+
+module.exports = function(artifacts) {
+  const getContract = artifacts ? artifacts.require.bind(artifacts) : Contracts.getFromLocal.bind(contract);
+  const getResultContract = artifacts ? artifacts.require.bind(artifacts) : contract.fromArtifact.bind(contract);
+
+  if (!artifacts) {
+    ZWeb3.initialize(ozWeb3.currentProvider);
+  }
+  const WhitelistedTokenSale = getContract('WhitelistedTokenSale');
+  const TokenSaleRegistry = getContract('TokenSaleRegistry');
+
+  WhitelistedTokenSale.numberFormat = 'String';
+
+  return {
+    async deployWhitelistedTokenSale(_tokenToSell, from, proxyAdmin) {
+      const tokenSaleProject = new SimpleProject('TokenSale', null, { from });
+
+      const tokenSaleRegistry = await tokenSaleProject.createProxy(TokenSaleRegistry, {
+        initArgs: [from],
+        admin: proxyAdmin
+      });
+      const tokenSale = await tokenSaleProject.createProxy(WhitelistedTokenSale, {
+        initArgs: [from, _tokenToSell, tokenSaleRegistry.address, true],
+        admin: proxyAdmin
+      });
+      return {
+        tokenSaleRegistry: await getResultContract('TokenSaleRegistry').at(tokenSaleRegistry._address),
+        tokenSale: await getResultContract('WhitelistedTokenSale').at(tokenSale._address)
+      };
+    }
+  };
+};
